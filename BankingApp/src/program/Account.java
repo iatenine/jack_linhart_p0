33fd0cb92237aaded7fefe08bb1c9dfe269157dd
Jack <iatenine@gmail.com>
2022-01-10 09:47:55 -0700

    Corrected Double Rounding Errors Closes #1

--- BankingApp/src/program/Account.java
+++ BankingApp/src/program/Account.java
@@ -15,21 +15,21 @@ public class Account {
 
     // deposit
     public void deposit(double deposit){
-        BigDecimal decimal;
-        this.amount += deposit;
+        this.amount = getPreciseDoubleAddition(getBalance(), deposit);
     }
 
     // withdraw
     public double withdraw(double request){
-        if(request > getBalance())
+        double newAmount = getPreciseDoubleSubtraction(getBalance(), request);
+        if(newAmount < 0)
             return 0;
-        amount -= request;
+        this.amount = newAmount;
         return request;
     }
 
     // Getter/Setters
     public double getBalance(){
-        return amount;
+        return this.amount;
     }
 
     public void setBalance(double newValue){ amount = newValue; }
@@ -41,4 +41,17 @@ public class Account {
         return this.name + ": " + df.format(this.getBalance());
     }
 
+    private double getPreciseDoubleAddition(double num1, double num2){
+        BigDecimal d1 = BigDecimal.valueOf(num1);
+        BigDecimal d2 = BigDecimal.valueOf(num2);
+        BigDecimal dFinal = d1.add(d2);
+        return dFinal.doubleValue();
+    }
+
+    private double getPreciseDoubleSubtraction(double num1, double num2){
+        BigDecimal d1 = BigDecimal.valueOf(num1);
+        BigDecimal d2 = BigDecimal.valueOf(num2);
+        BigDecimal dFinal = d1.subtract(d2);
+        return dFinal.doubleValue();
+    }
 }
