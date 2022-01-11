b2c0c63e1a920e6327e3d11e1d1039db2d79bf27
Jack <iatenine@gmail.com>
2022-01-11 12:11:17 -0700

    Add JavaDocs to MockDB

--- BankingApp/src/data/MockDb.java
+++ BankingApp/src/data/MockDb.java
@@ -3,15 +3,34 @@ package data;
 import program.List;
 import program.User;
 
-import java.util.Objects;
+/**
+ * @author Provides a mock data layer until a real database is connected
+ */
 
 public class MockDb {
+    /**
+     * users: Dynamic List of Users
+     */
     final private List<User> users = new List<>();
 
+    /**
+     * Adds user to users List
+     * @param user
+     * user to add
+     */
     public void addUser(User user){
         users.push(user);
     }
 
+    /**
+     * Attempt to login to a User stored in the users List
+     * @param username
+     * username to query data layer for
+     * @param password
+     * password to authenticate user with given username
+     * @return
+     * User object matching username and password input if found in users List
+     */
     public User login(String username, String password){
         Object[] allUsers = users.getItems();
         User ret = null;
