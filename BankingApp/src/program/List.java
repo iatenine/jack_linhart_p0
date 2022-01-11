d973f83a1a5ac4878ddea596cd2b9c6460551be3
Jack <iatenine@gmail.com>
2022-01-11 11:02:04 -0700

    Add JavaDocs to List

--- BankingApp/src/program/List.java
+++ BankingApp/src/program/List.java
@@ -2,26 +2,49 @@ package program;
 
 // Implement Link List to hold dynamic collection of Accounts
 
+/**
+ * Holds a dynamically resized array of any object
+ * @param <T>
+ * Object type to be held in list
+ */
+
 public class List<T> {
     final private int step = 5;
     private int tail = 0;
     private T[] list = (T[]) new Object[step];
 
     // Add an account
-    public void push(T account){
+
+    /**
+     * Add an item to the tail of the list
+     * @param item
+     * item to be added to the list
+     */
+    public void push(T item){
         if(tail == list.length - 1)
             grow();
-        list[tail] = account;
+        list[tail] = item;
         tail++;
     }
 
+    /**
+     * Remove and return the tail item of the list
+     * @return
+     * Removed item
+     */
     public T pop(){
         if(tail > 0)
             tail--;
         return list[tail];
     }
 
-    // Remove account if found
+    /**
+     * Remove item at a specified index
+     * @param index
+     * Index of item to remove
+     * @return
+     * Removed item
+     */
     public T removeItem(int index){
         if(index < 0 || index > list.length - 1)
             return null;
@@ -30,6 +53,11 @@ public class List<T> {
         return a;
     }
 
+    /**
+     * Get all items
+     * @return
+     * Array of all stored items
+     */
     public T[] getItems(){
         int blocks = 0;
         for (T a: list) {
@@ -46,6 +74,9 @@ public class List<T> {
         return a;
     }
 
+    /**
+     * Grow list's size
+     */
     private void grow(){
         T[] temp = list;
         list = (T[]) new Object[temp.length + step];
